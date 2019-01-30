c6b993a4057a3e7c35056a259b0fc08b63364120
Adit Jain <aditjain1980@gmail.com>
2019-01-30 11:46:15 +0530

    Update img.py

--- img.py
+++ img.py
@@ -7,9 +7,9 @@ path="~/Documents/cod/"
 kernel=np.ones((5,5),np.uint8)
 image=cv2.imread("45.png")
 hsvimage=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-def win_col(max_value,min_val,name):
+def win_col(max_value,min_value,name):
     global image,hsvimage,kernel
-    mask = cv2.inRange(hsvimage, max_value, min_val)
+    mask = cv2.inRange(hsvimage, min_value, max_value)
     mask=cv2.erode(mask,kernel,iterations=1)
     image1=cv2.bitwise_and(image,image,mask=mask)
     cnts,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -33,8 +33,9 @@ def win_col(max_value,min_val,name):
     cv2.imwrite(name+'.jpg', image1)
     cv2.imshow("image",image)
     cv2.imshow(name+ " Coloured Objects",image1)
+    input()
 win_col(np.array([179,255,255]),np.array([170,0,0]),"Red")
 win_col(np.array([86,255,255]),np.array([76,0,0]),"Green")
-win_col(np.array([23,60,0]),np.array([31,255,255]),"Yellow")
+win_col(np.array([31,255,255]),np.array([23,60,0]),"Yellow")
 win_col(np.array([179,255,255]),np.array([170,0,0]),"Blue")
-win_col(np.array([10,96,0]),np.array([15,255,255]),"Orange")
+win_col(np.array([15,255,255]),np.array([10,96,0]),"Orange")
